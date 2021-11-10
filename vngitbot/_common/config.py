5656f9395a5e3ee503cd1c344dedb613250a5ab4
wreckitkenny <tung.tran.3295@gmail.com>
2021-11-10 18:05:30 +0700

    Update basicConfig

--- vngitbot/_common/config.py
+++ vngitbot/_common/config.py
@@ -1,24 +1,24 @@
 import os, configparser, logging, gitlab
 
 class BasicConfig:
-    def __init__(self):
-        self.configPath = os.getenv('GITBOT_CONFIG_PATH')
-        # configPath = 'D:\\Code\\7.VNPAY\\CI\\gitbot\\gitbot.conf'
-        self.binPath = '/'.join(self.configPath.split('/')[:-1])
+    # def __init__(self):
+    configPath = os.getenv('GITBOT_CONFIG_PATH')
+    # configPath = 'D:\\Code\\7.VNPAY\\CI\\gitbot\\gitbot.conf'
+    binPath = '/'.join(configPath.split('/')[:-1])
 
-        # Config file module
-        self.parser = configparser.ConfigParser()
-        self.parser.read(self.configPath)
+    # Config file module
+    parser = configparser.ConfigParser()
+    parser.read(configPath)
 
-        # Gitlab module
-        self.gl = gitlab.Gitlab(self.parser.get('GITLAB', 'GITLAB_ADDRESS'), private_token=self.parser.get('GITLAB', 'GITLAB_TOKEN'))
+    # Gitlab module
+    gl = gitlab.Gitlab(parser.get('GITLAB', 'GITLAB_ADDRESS'), private_token=parser.get('GITLAB', 'GITLAB_TOKEN'))
 
     # Logging module
-    def logConfig(self):
-        level = self.parser.get('LOG', 'LOG_LEVEL')
+    def logConfig(parser):
+        level = parser.get('LOG', 'LOG_LEVEL')
         if level == 'INFO': lvl = logging.INFO
         if level == 'DEBUG': lvl = logging.DEBUG
-        logging.basicConfig(filename=self.parser.get('LOG', 'LOG_PATH')+'/'+self.parser.get('LOG', 'LOG_FILENAME'), 
+        logging.basicConfig(filename=parser.get('LOG', 'LOG_PATH')+'/'+parser.get('LOG', 'LOG_FILENAME'), 
                             level=lvl,
                             format='%(asctime)s | %(levelname)s | %(message)s',
                             datefmt='%d/%m/%Y | %I:%M:%S')
--- vngitbot/launch.py
+++ vngitbot/launch.py
@@ -24,9 +24,9 @@ def run(server_class=HTTPServer, handler_class=Webhook, addr="localhost", port=8
     httpd.serve_forever()
 
 if __name__ == "__main__":
-    parser = bc.parser
+    parser = bc.p
     bc.logConfig(parser)
-    
+
     # Declare Endpoint information
     endpointAddress = bc.parser.get('ENDPOINT', 'ENDPOINT_ADDRESS')
     endpointPort = bc.parser.get('ENDPOINT', 'ENDPOINT_PORT')
