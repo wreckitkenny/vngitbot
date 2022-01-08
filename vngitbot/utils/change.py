3ec27ce09925af5b0aaf679308777341ddd2c3a8
_wiky <tungtt4@vnpay.vn>
2022-01-08 21:02:50 +0700

    Update module

--- vngitbot/utils/change.py
+++ vngitbot/utils/change.py
@@ -1,5 +1,5 @@
 import os, logging, re, gitlab
-from merge import downloadOwnerFile, cacheProject
+from .merge import downloadOwnerFile, cacheProject
 
 def checkEnvironment(gl, parser, pushedTag):
     env = cdProject = ''
