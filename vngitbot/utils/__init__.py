2e4879541f009c75eb425432304ff46a7958bd1e
Wreck-it Kenny <tungtt4@vnpay.vn>
2023-10-05 10:47:30 +0700

    Removed deprecated functions

--- vngitbot/utils/__init__.py
+++ vngitbot/utils/__init__.py
@@ -1,5 +1,5 @@
 from .version import __version__
 from .config import BasicConfig
-from .change import checkDup, checkEnvironment, getOldTag, locateBlob, changeTag, cacheImage
+from .change import checkDup, checkEnvironment, getOldTag, locateBlob, changeTag
 
-__all__ = ['__version__', 'BasicConfig', 'checkDup', 'checkEnvironment', 'getOldTag', 'locateBlob', 'changeTag', 'cacheImage']
+__all__ = ['__version__', 'BasicConfig', 'checkDup', 'checkEnvironment', 'getOldTag', 'locateBlob', 'changeTag']
